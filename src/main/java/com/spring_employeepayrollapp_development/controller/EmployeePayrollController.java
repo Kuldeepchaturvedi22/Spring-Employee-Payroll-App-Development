8cc9cf06bd6de15917036c508307756f09d6fd1d
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-02-25 16:38:52 +0530

    [Kuldeep] Add. Use Lombok Library for Logging

--- src/main/java/com/spring_employeepayrollapp_development/controller/EmployeePayrollController.java
+++ src/main/java/com/spring_employeepayrollapp_development/controller/EmployeePayrollController.java
@@ -2,6 +2,7 @@ package com.spring_employeepayrollapp_development.controller;
 
 import com.spring_employeepayrollapp_development.model.EmployeeModel;
 import com.spring_employeepayrollapp_development.service.EmployeePayrollService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@Slf4j
 public class EmployeePayrollController {
 
     @Autowired
@@ -19,16 +21,19 @@ public class EmployeePayrollController {
     // GET all employees
     @GetMapping
     public ResponseEntity<List<EmployeeModel>> getAllEmployees() {
+        log.info("Fetching all employees");
         return new ResponseEntity<>(employeePayrollService.getAllEmployees(), HttpStatus.OK);
     }
 
     // GET employee by ID
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeModel> getEmployee(@PathVariable int id) {
+        log.info("Fetching employee with ID: {}", id);
         EmployeeModel employee = employeePayrollService.getEmployeeById(id);
         if (employee != null) {
             return new ResponseEntity<>(employee, HttpStatus.OK);
         } else {
+            log.warn("Employee with ID: {} not found", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -36,16 +41,19 @@ public class EmployeePayrollController {
     // POST - Create new employee
     @PostMapping
     public ResponseEntity<EmployeeModel> createEmployee(@RequestBody EmployeeModel employeeData) {
+        log.info("Creating new employee: {}", employeeData);
         return new ResponseEntity<>(employeePayrollService.createEmployee(employeeData), HttpStatus.CREATED);
     }
 
     // PUT - Update existing employee
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeModel> updateEmployee(@PathVariable int id, @RequestBody EmployeeModel employeeData) {
+        log.info("Updating employee with ID: {}", id);
         EmployeeModel updatedEmployee = employeePayrollService.updateEmployee(id, employeeData);
         if (updatedEmployee != null) {
             return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
         } else {
+            log.warn("Employee with ID: {} not found", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -53,9 +61,11 @@ public class EmployeePayrollController {
     // DELETE - Delete employee
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+        log.info("Deleting employee with ID: {}", id);
         if (employeePayrollService.deleteEmployee(id)) {
             return new ResponseEntity<>("ID " + id + " Deleted!", HttpStatus.OK);
         } else {
+            log.warn("Employee with ID: {} not found", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
