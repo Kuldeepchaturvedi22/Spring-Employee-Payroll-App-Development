65a80a138a6567e3e348a60ff52b29d8a625367a
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-02-25 15:48:12 +0530

    [Kuldeep] Add. Introducing DTO and Model to Employee Payroll App

--- src/main/java/com/spring_employeepayrollapp_development/controller/EmployeePayrollController.java
+++ src/main/java/com/spring_employeepayrollapp_development/controller/EmployeePayrollController.java
@@ -1,39 +1,60 @@
 package com.spring_employeepayrollapp_development.controller;
 
+import com.spring_employeepayrollapp_development.model.EmployeeModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeePayrollController {
+    private List<EmployeeModel> employeeList = new ArrayList<>();
+
     // GET all employees
     @GetMapping
-    public ResponseEntity<String> getAllEmployees() {
-        return new ResponseEntity<>("Get All Employees", HttpStatus.OK);
+    public ResponseEntity<List<EmployeeModel>> getAllEmployees() {
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
     }
 
     // GET employee by ID
     @GetMapping("/{id}")
-    public ResponseEntity<String> getEmployeeById(@PathVariable Long id) {
-        return new ResponseEntity<>("Get Employee by ID: " + id, HttpStatus.OK);
+    public ResponseEntity<EmployeeModel> getEmployee(@PathVariable int id) {
+        if (id >= 0 && id < employeeList.size()) {
+            return new ResponseEntity<>(employeeList.get(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     // POST - Create new employee
     @PostMapping
-    public ResponseEntity<String> createEmployee(@RequestBody Object employeeData) {
-        return new ResponseEntity<>("Create Employee", HttpStatus.CREATED);
+    public ResponseEntity<EmployeeModel> createEmployee(@RequestBody EmployeeModel employeeData) {
+        employeeList.add(employeeData);
+        return new ResponseEntity<>(employeeData, HttpStatus.CREATED);
     }
 
     // PUT - Update existing employee
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateEmployee(@PathVariable Long id, @RequestBody Object employeeData) {
-        return new ResponseEntity<>("Update Employee with ID: " + id, HttpStatus.OK);
+    public ResponseEntity<EmployeeModel> updateEmployee(@PathVariable int id, @RequestBody EmployeeModel employeeData) {
+        if (id >= 0 && id < employeeList.size()) {
+            employeeList.set(id, employeeData);
+            return new ResponseEntity<>(employeeData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     // DELETE - Delete employee
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
-        return new ResponseEntity<>("Delete Employee with ID: " + id, HttpStatus.OK);
+    public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+        if (id >= 0 && id < employeeList.size()) {
+            employeeList.remove(id);
+            return new ResponseEntity<>("ID "+id+" Deleted!",(HttpStatus.OK));
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/spring_employeepayrollapp_development/model/EmployeeModel.java
+++ src/main/java/com/spring_employeepayrollapp_development/model/EmployeeModel.java
@@ -1,6 +1,7 @@
 package com.spring_employeepayrollapp_development.model;
 
 public class EmployeeModel {
+    private int id;
     private String name;
     private double salary;
 
@@ -10,10 +11,16 @@ public class EmployeeModel {
     public void setSalary (double salary) {
         this.salary = salary;
     }
+    public void setId (int id) {
+        this.id = id;
+    }
     public String getName () {
         return name;
     }
     public double getSalary () {
         return salary;
+    }
+    public int getId () {
+        return id;
     }
 }
