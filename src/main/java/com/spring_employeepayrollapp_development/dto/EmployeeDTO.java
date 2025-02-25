612ab744ff23ac29aa7817417ae1e31b08e89caa
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-02-25 15:19:38 +0530

    [Kuldeep] Add. Introducing DTO and Model to Employee Payroll App

---
+++ src/main/java/com/spring_employeepayrollapp_development/dto/EmployeeDTO.java
@@ -0,0 +1,19 @@
+package com.spring_employeepayrollapp_development.dto;
+
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+
+    public void setName (String name) {
+        this.name = name;
+    }
+    public void setSalary (double salary) {
+        this.salary = salary;
+    }
+    public String getName () {
+        return name;
+    }
+    public double getSalary () {
+        return salary;
+    }
+}
